c9a1072a1067cf7cbe3f117b7f2eb1e8f1dddf2b
Samia <48843553+Samia1117@users.noreply.github.com>
2022-07-07 02:02:26 +0000

    Sync LeetCode submission Runtime - 25 ms (0.22%), Memory - 13.4 MB (2.85%)

--- solutions/0226-invert-binary-tree/solution.py
+++ solutions/0226-invert-binary-tree/solution.py
@@ -1,47 +1,23 @@
-#Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        
         if root == None:
             return None
-        invertedLeft = self.invertTree(root.left)
         invertedRight = self.invertTree(root.right)
-        
+        invertedLeft = self.invertTree(root.left)
         root.left = invertedRight
         root.right = invertedLeft
-        
         return root
-        '''
-        if root.left == None:
-            if root.right==None:  # leaf node
-                return root
-            else:
-                right_subtree = self.invertTree(root.right)
-                root.left = right_subtree # only right side not empty
-                root.right = None
-                return root
-        if root.right == None:  # only left side not empty
-            left_subtree = self.invertTree(root.left)
-            root.right = left_subtree # left not None
-            root.left = None
-            return root
         
-        if root.left.left == None and root.left.right == None and root.right.right == None and root.right.left==None:
-            temp = root.left
-            root.left = root.right
-            root.right = temp
-            return root
             
-        else:
-            my_left = self.invertTree(root.left)
-            my_right = self.invertTree(root.right)
             
-            temp = my_left
-            root.left = my_right
-            root.right = temp
-            return root
-        '''
