2ee7149b555d16f33a63933283e8d5b19641bd26
Samia <48843553+Samia1117@users.noreply.github.com>
2022-07-20 04:40:58 +0000

    Sync LeetCode submission Runtime - 31 ms (0.50%), Memory - 13.4 MB (20.13%)

---
+++ solutions/0070-climbing-stairs/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+        
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0:
+            return 0
+        
+        if n==2 or n==1:
+            return n
+        
+        dp = [0 for _ in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
+        
+        
