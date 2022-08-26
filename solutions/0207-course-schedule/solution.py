ab8f2a7499154adea8bcddda8cdf5ba673e55721
Samia <48843553+Samia1117@users.noreply.github.com>
2022-08-26 19:10:05 +0000

    Sync LeetCode submission Runtime - 157 ms (5.06%), Memory - 17.1 MB (100.00%)

---
+++ solutions/0207-course-schedule/solution.py
@@ -0,0 +1,73 @@
+class Solution:
+    
+    def dfsCycle(self, adjDict, node, visited, recStack):
+        
+        visited.add(node)
+        recStack.add(node)
+        
+        for neighbor in adjDict[node]:
+            #print("neighbor is: ", neighbor)
+            if neighbor in recStack:
+                return True
+            if neighbor not in visited:
+                if self.dfsCycle(adjDict, neighbor, visited, recStack):
+                    return True
+        
+        #print("recStack, visited were: ", [recStack, visited])
+        #print("## Done with current path")
+        recStack.remove(node)
+        return False
+        
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # treat prerequisites[i] as an edge of a graph
+        # if graph has cycle, then can not finish
+        
+        # check for cycle: if node refers to one of its ancestors,
+        # then there is a cycle. 
+        # Ancestor: in the same path as the current dfs
+        
+        if prerequisites == []:
+            return True
+        
+        adjDict = {}
+        hasPrereq = set()
+        
+        for p in prerequisites:
+            prereq = p[1]
+            other = p[0]
+            
+            if prereq == other: # self-loop
+                return False
+            
+            if prereq not in adjDict:
+                adjDict[prereq] = []
+            adjDict[prereq].append(other)
+            
+            if other not in adjDict:
+                adjDict[other] = []
+                
+            hasPrereq.add(other)
+            
+        allNodes = adjDict.keys()
+        
+        roots = list(set(allNodes) - hasPrereq)
+        if len(roots) == 0:
+            return False
+        
+        megaVisited = set()
+        # Now find a root/vertex with in-degree = 0
+        for root in roots:
+            recStack = set([root])
+            visited = set([root])
+            
+            # Now dfs
+            for neighbor in adjDict[root]:
+                if self.dfsCycle(adjDict, neighbor, visited, recStack):
+                    return False
+            megaVisited.update(visited)
+        
+        if len(megaVisited) != len(allNodes):
+            return False
+        return True
+        
