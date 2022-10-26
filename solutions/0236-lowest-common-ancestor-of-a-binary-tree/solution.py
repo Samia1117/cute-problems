a34be52faee9b37ecab878d695843ae61801c2fa
Samia <48843553+Samia1117@users.noreply.github.com>
2022-10-26 15:36:36 +0000

    Sync LeetCode submission Runtime - 156 ms (5.28%), Memory - 26.3 MB (5.56%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -6,7 +6,6 @@
 #         self.right = None
 
 class Solution:
-        
         # main idea is: if my right and left both return not none values, then either p or q is on right and the other on left
         # in that case, i should return the  root
 
@@ -14,14 +13,37 @@ class Solution:
         
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if root in (p, q, None):
+        if not root:
             return root
-        r,l = [self.lowestCommonAncestor(child, p, q) for child in (root.left, root.right)]
+        if root.val == p.val or root.val == q.val:
+            return root
         
-        if r and l:
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
             return root
-        if not r:
-            return l
-        return r
+        if left:
+            return left
+        return right
+        
+#         if root in (p, q, None):
+#             return root
+#         r,l = [self.lowestCommonAncestor(child, p, q) for child in (root.left, root.right)]
+        
+#         if r and l:
+#             return root
+#         if not r:
+#             return l
+#         return r
+        
+#         st = deque()
+#         st.append(root)
+#         visited = set()
+        
+#         while st:
+#             p = st.pop()
+            
+            
         
         
