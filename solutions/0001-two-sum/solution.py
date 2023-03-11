40554c68c2c1bbf012c00c7ff95cbfbc7caf5721
Samia <48843553+Samia1117@users.noreply.github.com>
2023-03-11 23:21:30 +0000

    Sync LeetCode submission Runtime - 42 ms (44.06%), Memory - 14.4 MB (17.63%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,15 +1,20 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        hashMap = {}
-        
-        for i in range(len(nums)):
-            num = nums[i]
-            if num in hashMap:
-                return [hashMap[num], i]
-            want = target-num
-            hashMap[want] = i
-        
-        
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        d = {}
+        i = 0
+        for num in nums:
+            keyNeeded = target - num
+            # print("key needed is: ", keyNeeded)
+            if keyNeeded in d:
+                return [d[keyNeeded], i]
+            d[num] = i
+            # print("d is: ", d)
+            i +=1
             
-        
+        return []
