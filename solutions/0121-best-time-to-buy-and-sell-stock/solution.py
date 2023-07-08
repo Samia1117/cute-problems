83ea218ea99598f653292ca4d02ed185f89ac5d2
Samia <48843553+Samia1117@users.noreply.github.com>
2023-07-08 00:12:46 +0000

    Sync LeetCode submission Runtime - 869 ms (5.09%), Memory - 22.5 MB (12.99%)

--- solutions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -4,80 +4,14 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
-        # currentMax = 0
-        # for i in range(len(prices)-1):
-        #     buyVal = prices[i]
-        #     sellValMax = sorted(prices[i+1:], reverse=True)[0]
-        #     maxProfit = sellValMax - buyVal
-        #     if maxProfit > currentMax:
-        #          currentMax = maxProfit
-        #     # print("Current max profit was: ", maxProfit)
-        # return currentMax
-        
-        # for each day see it as a 'sell day', and
-        # keep a 'thisDayMaxProfit' variable for that day
-        # if the next day is even more profitable to sell
-        # then we can add this day's highest profit to the
-        # difference: nextDaySellVal-thisDaySellVal
-        
+        if len(prices) <=1:
+            return 0
 
-        
-#         # maxProfitSellDay = 0
-        
-#         maxSellPrice = prices[0]
-#         maxProfit = 0
-        
-#         for i in range(1, len(prices)):
-#             print("Current sell price proposal ", prices[i])
-#             print("Current max sell price: ", maxSellPrice)
-#             if prices[i] > maxSellPrice:
-#                 print("Found better sell price = ", prices[i])
-#                 maxProfit += prices[i] - maxSellPrice
-#                 maxSellPrice = prices[i]
-#                 print("Improved profit to : ", maxProfit)
-#                 print("Changed Sell Price to : ", maxSellPrice)
-                
-#             else:    # if I'm not improving profit
-#                 if maxProfit == 0:
-#                     maxSellPrice = min(maxSellPrice, prices[i])
-#                     print("Changed Max Sell Price to: ", maxSellPrice)
-#         return maxProfit
-
-        diffarr=[]
-        prev=prices[0]
-        for i in range(len(prices)):
-            diffarr.append(prices[i]-prev)
-            prev=min(prices[i],prev) 
-        return max(diffarr)
-
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+        maxProfit = 0
+        anchor = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] - anchor > maxProfit:
+                maxProfit = prices[i] - anchor
+            if prices[i] < anchor:
+                anchor = prices[i]
+        return maxProfit
