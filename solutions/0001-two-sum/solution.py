90302ce4a126769310644c59a2c2fd4717cd4a12
Samia <48843553+Samia1117@users.noreply.github.com>
2023-07-08 00:00:07 +0000

    Sync LeetCode submission Runtime - 51 ms (44.03%), Memory - 15 MB (17.63%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -10,11 +10,18 @@ class Solution(object):
         i = 0
         for num in nums:
             keyNeeded = target - num
-            # print("key needed is: ", keyNeeded)
-            if keyNeeded in d:
-                return [d[keyNeeded], i]
-            d[num] = i
-            # print("d is: ", d)
+            #if keyNeeded in d:
+                #return [d[keyNeeded], i]
+            #d[num] = i
+            d[keyNeeded] = [i] # i is the index of one of the numbers
             i +=1
-            
+        
+        i = 0
+        for num in nums:
+            if num in d:
+                if i!=d[num][0]:
+                    #print("i, d[num] ", [i, d[num][0]])
+                    d[num].append(i)
+                    return d[num]
+            i +=1
         return []
