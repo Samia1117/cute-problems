461dfb478d6976aadf8585c814117ef46a785d75
Samia <48843553+Samia1117@users.noreply.github.com>
2024-04-22 12:14:30 +0000

    Sync LeetCode submission Runtime - 56 ms (5.06%), Memory - 17.7 MB (99.99%)

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,19 +1,38 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
 
-        if len(prices) <= 1:
-            return 0
+        totalProfit = 0
+        n = len(prices)
+        boughtAt = None
 
-        anchor = prices[0]
-        sumProfit = 0
-        for i in range(1, len(prices)):
-            if (prices[i] - anchor > 0):
-                sumProfit += prices[i] - anchor
-            anchor = prices[i]
+        for i in range(n):
+            if i < n-1:
+                if prices[i+1] <= prices[i]: # decreasing, so sell
+                    if boughtAt != None:
+                        # sell
+                        print("Bought at ", boughtAt)
+                        print("Sold at", prices[i])
+
+                        profit = prices[i] - boughtAt
+                        print("Profit: ", profit)
+
+                        totalProfit += profit
+                        # buy later
+                        boughtAt = None
+                    continue
+                if boughtAt == None:
+                    boughtAt = prices[i]
+                elif prices[i] < boughtAt:
+                    boughtAt = prices[i]
+
+                # totalProfit += profit
+
+                # boughtAt = prices[i]
+            if i == n-1:
+                if boughtAt != None:
+                    totalProfit += prices[i] - boughtAt
+                    
+        return totalProfit
+            
+
         
-        return sumProfit
-
