c351f165ad2327a39018be2157697b3fc0338029
Samia <48843553+Samia1117@users.noreply.github.com>
2024-04-25 02:07:58 +0000

    Sync LeetCode submission Runtime - 247 ms (51.65%), Memory - 19 MB (99.98%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -1,51 +1,64 @@
 class Solution:
-    def offGrid(self, row, col, numRows, numCols):
-        if (row >= numRows or col >= numCols
-        or row < 0 or col < 0):
-            return True
-        return False
-    
-    def getPos(self, row, col, maxCol):
-        return maxCol*row + col
-    
-    def markVisitedDFS(self, row, col, grid, numRows, numCols, posVisited):
-        pos = self.getPos(row, col, numCols)
-        if pos not in posVisited:
-            posVisited.add(pos)
-            # print("Now marking visited: ", [row, col])
-            neigh_index = [[0,1], [1, 0], [-1, 0], [0, -1]]
-            for i in range(4):
-                x = neigh_index[i][0]
-                y = neigh_index[i][1]
-                if not self.offGrid(row+x, col+y, numRows, numCols):
-                    if grid[row+x][col+y] == "1":
-                        self.markVisitedDFS(row+x, col+y, grid, numRows, numCols, posVisited)
-    
+    def idx(self, row, col, maxRow, maxCol):
+        return (row * maxCol) + col
+
+    def is_in_bounds(self, i, j, maxRow, maxCol):
+        return i >= 0 and j >= 0 and i < maxRow and j < maxCol
+
+    def dfs_and_mark(self, visited, grid, i, j, maxRow, maxCol):
+        st = deque()
+        st.append([i,j])
+
+        # print("i, j: ", [i,j])
+        while st:
+            popped = st.pop()
+            visited[popped[0]][popped[1]] = True
+            # print("New visited: ", visited)
+            # left
+            left_i, left_j = popped[0], popped[1] - 1
+            # print("left i, j: ", [left_i, left_j])
+            if self.is_in_bounds(left_i, left_j, maxRow, maxCol) and grid[left_i][left_j] == '1' and not visited[left_i][left_j]:
+                st.append([left_i, left_j])
+
+            # right
+            right_i, right_j = popped[0], popped[1] + 1
+            # print("right i, j: ", [right_i, right_j])
+            if self.is_in_bounds(right_i, right_j, maxRow, maxCol) and grid[right_i][right_j] == '1' and not visited[right_i][right_j]:
+                st.append([right_i, right_j])
+
+            # up
+            up_i, up_j = popped[0] - 1, popped[1]
+            # print("up i, j: ", [up_i, up_j])
+            if self.is_in_bounds(up_i, up_j, maxRow, maxCol) and grid[up_i][up_j] == '1' and not visited[up_i][up_j]:
+                st.append([up_i, up_j])
+
+            # down
+            down_i, down_j = popped[0] + 1, popped[1]
+            # print("down i, j: ", [down_i, down_j])
+            if self.is_in_bounds(down_i, down_j, maxRow, maxCol) and grid[down_i][down_j] == '1' and not visited[down_i][down_j]:
+                st.append([down_i, down_j])
+            
+        return
+
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        if grid == []:
+
+        maxRow = len(grid)
+        if maxRow == 0:
             return 0
-        
-        numRows = len(grid)
-        numCols = len(grid[0])
-        posVisited = set([])
-        
-        islandCount = 0
-        
-        for i in range(numRows):
-            for j in range(numCols):
-                pos = self.getPos(i, j, numCols)
-                if pos not in posVisited:
-                    if grid[i][j] == "1":
-                        # print("Found a non visited one")
-                        islandCount +=1
-                        self.markVisitedDFS(i, j, grid, numRows, numCols, posVisited)   # mark all neighboring 1's as visited
-                else:
-                    posVisited.add(pos)
-                # print("Positions visited: ", posVisited)
-                      
-        return islandCount
+        maxCol = len(grid[0])
+
+        visited = [[False for i in range(maxCol)] for j in range(maxRow)]
+
+        conn_components = 0
+        for i in range(maxRow):
+            for j in range(maxCol):
+
+                # check connnected components 
+                if grid[i][j] == '1' and not visited[i][j]:
+                    # print("found land at: ", [i, j])
+                    self.dfs_and_mark(visited, grid, i, j, maxRow, maxCol)
+                    conn_components += 1
+                # print("One connected component down. I, j: ", [i,j])
                         
-                        
-                        
+        return conn_components
         
