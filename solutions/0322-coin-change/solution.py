91bb3ff552e258ee1252f78afc6b93c6285ad006
Samia <48843553+Samia1117@users.noreply.github.com>
2024-05-02 01:28:13 +0000

    Sync LeetCode submission Runtime - 1170 ms (11.49%), Memory - 18.7 MB (64.67%)

--- solutions/0322-coin-change/solution.py
+++ solutions/0322-coin-change/solution.py
@@ -1,47 +1,61 @@
+import sys
+
 class Solution:
-    
-    def minCoins(self, coins, amount, numCoins):
-        if amount<0:
-            return None
-        if amount ==0:
-            return numCoins
-        else:
-            currentMin = 10000
-            for coin in coins:
-                minCs = self.minCoins(coins, amount-coin, numCoins+1)
-                if minCs == None:
-                    continue
-                if minCs < currentMin:
-                    currentMin = minCs
-            return currentMin
-        
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        dp = [100003 for i in range(amount+1)]
-        
-        dp[0] = 0   # 0 coins for amount = 0
-        
-        for currentAmount in range(amount+1):
-            for c in coins:
-                if c+currentAmount <= amount:
-                    dp[c+currentAmount] = min(dp[c+currentAmount], dp[currentAmount] + 1)
-                
-        n = len(dp)-1
-        if dp[n] == 100003:
+        if not coins or amount < 0:
             return -1
-        return dp[n]
+
+        # dp[i][j] = min number of coins I need to make j amount using first i coins
+        # dp[n][amount] = min number of coins I need to make 'amount' amount using first n (all) coins
+
+        # base case
+        # dp[i][0] = 0, since no coins needed to make amount 0
+        # dp[0][j] = inf, since cannot make any amount j unless 
+
+        # transition:
+        # dp[i-1][j-coins(i)] = min # of coins to make j-coins using first i-1 coins - assumes we use i^th coin 
+        # dp[i-1][j] = min # of coins to make j using first i coins - assumes we DON'T use ith coin 
+        # dp[i][j] = min(dp[i-1][j], 
+        n = len(coins)
+
+        dp = [ [10001 for j in range(amount + 1)] for i in range(n+1)]
+
+        for i in range(n+1):
+            for j in range(amount+1):
+                if j == 0:
+                    dp[i][j] = 0
+                    continue
+                
+                elif i == 0:
+                    continue
+
+                # print("i, j: ", [i, j])
+                # print("DP so far: ", dp)
+                
+                # 1. Don't use coin i
+                best_without_coin_i = dp[i-1][j] 
+
+                # Note coins index is 1 lower
+                amount_without_coin_i = j - coins[i-1]
+
+                # Check if coin i can be used to make amount j
+                if amount_without_coin_i >= 0: 
+                    # 2. Use coin i (at index i-1) 
+                    best_with_coin_i = dp[i][amount_without_coin_i] + 1
+
+                    dp[i][j] = min(best_without_coin_i, best_with_coin_i)
+                else:
+                    dp[i][j] = best_without_coin_i
+
         
-        # if amount == 0:
-        #     return 0
+        if dp[n][amount] == 10001:
+            return -1
+        return dp[n][amount]
+
         
-#         currentMin = 10000
-#         for coin in coins:
-#             minCs = self.minCoins(coins, amount-coin, 1)
-#             if minCs == None:
-#                 continue
-#             if minCs < currentMin:
-#                 currentMin = minCs
+
+
+
         
-#         if currentMin == 10000:
-#             return -1
-#         return currentMin
+
+        
