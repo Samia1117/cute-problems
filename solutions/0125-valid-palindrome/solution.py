bea5e8781fd5d7230afc235cc5f5328fdb8b4302
Samia <48843553+Samia1117@users.noreply.github.com>
2024-09-12 12:11:44 +0000

    Sync LeetCode submission Runtime - 42 ms (5.70%), Memory - 17 MB (100.00%)

--- solutions/0125-valid-palindrome/solution.py
+++ solutions/0125-valid-palindrome/solution.py
@@ -1,31 +1,42 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        alphset = set("abcdefghijklmnopqrstuvwxyz")
-        alphset2 = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        digset = set("0123456789")
-        # print(alphset)
-        # print(digset)
-        
-        s2 = ""
-        for ch in s:
-            if ch in digset or ch in alphset:
-                s2 += ch
-            elif ch in alphset2:
-                s2 += ch.lower()
-        # print(s2)
-        
-        while s2:
-            if len(s2) >= 2:
-                if s2[0] == s2[-1]:
-                    s2 = s2[1:-1]
-                else:
-                    return False
-            else:
-                break
+class Solution:
 
+    def helperIsPalindrome(self, s: str) -> bool:
+        if len(s) <= 1:
+            return True
+        
+        if s[0] == s[-1]:
+            # print(f"New s={s[1:-1]}")
+            return self.helperIsPalindrome(s[1:-1])
+        else:
+            return False
+        
+    def isPalindrome(self, s: str) -> bool:
+
+        if len(s) <= 1:
+            return True
+
+        aset = set('abcdefghijklmnopqrstuvwxyz0123456789')
+        cset = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+        stripped = ""
+        for ch in s:
+            if ch in aset:
+                stripped += ch
+            elif ch in cset:
+                stripped += ch.lower()
+        
+
+        left, right = 0, len(stripped)
+        while right - left >= 2:
+            # print(f"New s={stripped[left:right-1]}")
+            if stripped[left] == stripped[right-1]:
+                left += 1
+                right -= 1
+            else:
+                # print(f"False because s={stripped[left:right-1]}")
+                return False
         return True
 
+
+        # print(f"Stripped = {stripped}")
+        # return self.helperIsPalindrome(stripped)
