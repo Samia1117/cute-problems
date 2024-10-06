546c60c722c94742187c257405f56f2bdf38be76
Samia <48843553+Samia1117@users.noreply.github.com>
2024-10-06 17:54:19 +0000

    Sync LeetCode submission Runtime - 71 ms (5.19%), Memory - 21.1 MB (99.99%)

--- solutions/0124-binary-tree-maximum-path-sum/solution.py
+++ solutions/0124-binary-tree-maximum-path-sum/solution.py
@@ -4,32 +4,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class maxSum:
-    def __init__(self):
-        self.val = -1000
-        
 class Solution:
-    def maxPathSum2(self, root, maxSum):
-        if root == None:
-            return 0
+
+    def helper(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return -10000, -10000
+        # calculate pathsums based on whether input root was used or not
+        use_root_rsum, not_root_rsum = self.helper(root.right)
+        use_root_lsum, not_root_lsum = self.helper(root.left)
+
+        # calculate max possible without using current root:
+        max_with_root = max( 
+            (max(use_root_rsum, use_root_lsum) + root.val), 
+            root.val)
+        # calc max value without using current root
+
+        max_wo_root = max(
+            # best you could do with child nodes
+            max (
+                max(use_root_rsum, not_root_rsum),
+                max(use_root_lsum, not_root_lsum)
+            ),
+            # best you can do with both children plus yourself
+            (root.val + use_root_rsum + use_root_lsum)
+        )
+
+        return max_with_root, max_wo_root
+
         
-        maxL = self.maxPathSum2(root.left, maxSum)
-        maxR =  self.maxPathSum2(root.right, maxSum)
-        if maxR<0:
-            maxR = 0
-        if maxL<0:
-            maxL = 0
-            
-        if maxL+maxR+root.val >maxSum.val:
-            maxSum.val = maxL+maxR+root.val
-        return max(maxL+root.val, maxR+root.val)
-                        
-    def maxPathSum(self, root: TreeNode) -> int:
-        if root.right == None and root.left == None:
-            return root.val
-        ms = maxSum()
-        othermax = self.maxPathSum2(root, ms)
-        print(othermax)
-        return ms.val
-            
-        
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+
+        a,b = self.helper(root)
+        return max(a, b)
+
+        '''
+
+        if not root:
+            return -10000
+
+        right_pathsum = self.maxPathSum(root.right)
+        left_pathsum = self.maxPathSum(root.left)
+
+        total_pathsum = root.val + right_pathsum + left_pathsum
+
+        if root.val < 0:
+            return max(max(total_pathsum, right_pathsum), left_pathsum)
+        else:
+            right_and_root = root.val + right_pathsum
+            left_and_root = root.val + left_pathsum
+            right_left_and_root = root.val + right_pathsum + left_pathsum
+
+            return max(max(right_and_root, left_and_root), right_left_and_root)
+            '''
