e4394897f0572c5e0113114188af8b58752f5594
Samia <48843553+Samia1117@users.noreply.github.com>
2024-10-08 23:35:25 +0000

    Sync LeetCode submission Runtime - 134 ms (5.01%), Memory - 20.7 MB (84.53%)

--- solutions/0056-merge-intervals/solution.py
+++ solutions/0056-merge-intervals/solution.py
@@ -1,22 +1,31 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
-        intervals = sorted(intervals, key=lambda x: x[0])
+
         n = len(intervals)
-        if n==0 or n==1:
+
+        if n == 0 or n == 1:
             return intervals
         
-        ret = []
-        newInterval = intervals[0]
-        ret.append(newInterval)
+        intervals = sorted(intervals, key=lambda x:x[1])
+        intervals = sorted(intervals, key=lambda x:x[0])
+        merged_intervals = []
+
+        i = 0
+        while i < n:
+            interval_to_merge = intervals[i]
+            # while endTime of this interval exceeds the startTime of next interval, don't add it to final list; keep merging
+            while i < n-1 and interval_to_merge[1] >= intervals[i+1][0]:
+                # overlap
+                interval_to_merge[1] = max(interval_to_merge[1], intervals[i+1][1])
+                i += 1
+
+            merged_intervals.append(interval_to_merge)
+            i += 1
+
+        return merged_intervals
+
+
+
+
         
-        for interval in intervals:
-            if interval[0] <= newInterval[1]:
-                newInterval[1] = max(newInterval[1], interval[1])
-            else:
-                newInterval = interval
-                ret.append(newInterval)
         
-        return ret
-    
-            
