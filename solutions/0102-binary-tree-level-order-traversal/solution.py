cb3544b7c065c0643fa857a0526cd2916be5a464
Samia <48843553+Samia1117@users.noreply.github.com>
2024-11-10 20:18:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (98.09%)

--- solutions/0102-binary-tree-level-order-traversal/solution.py
+++ solutions/0102-binary-tree-level-order-traversal/solution.py
@@ -5,51 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def dfs(self, v, depth, depthMap):
-        if v==None:
+    def populate_dict(self, root, level_dict, level):
+
+        if not root:
             return
-        if depth not in depthMap:
-            depthMap[depth] = []
-            
-        depthMap[depth].append(v.val)
-        self.dfs(v.left, depth+1, depthMap)
-        self.dfs(v.right, depth+1, depthMap)
-        return
-    
+
+        if level not in level_dict:
+            level_dict[level] = []
+        
+        level_dict[level].append(root.val)
+
+        self.populate_dict(root.left, level_dict, level+1)
+        self.populate_dict(root.right, level_dict, level+1)
+        
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        if root == None:
-            return []
-        
-        dMap = {}
-        self.dfs(root, 0, dMap) # the map gets populated and returned
-        
-#         # items = sorted(dMap.items(), key= lambda x:x[0])
-#         # return [i[1] for i in items]
-        return [dMap[k] for k in sorted(dMap)]
-        
-#         q = deque()
-#         q.append(root)
-#         result = []
-        
-#         while q:
-#             nextLevel = []  
-#             for i in range(len(q)):
-#                 popped = q.popleft()
-#                 nextLevel.append(popped.val)
-                
-#                 left = popped.left
-#                 right = popped.right
-                          
-#                 if left !=None:
-#                     q.append(left)
-#                 if right !=None:
-#                     q.append(right)
-#             result.append(nextLevel)
+        level_dict = {}
+        self.populate_dict(root, level_dict, 0)
+        sorted_by_level = sorted(level_dict.items(), key=lambda x: x[0])
+
+        return [x[1] for x in sorted_by_level]
 
         
-#         return result
-            
-            
+
+
         
