c31ffcffa561d0dc6ca0a70a547581ca3d629c7d
Samia <48843553+Samia1117@users.noreply.github.com>
2024-12-02 02:53:28 +0000

    Sync LeetCode submission Runtime - 2 ms (100.00%), Memory - 46.1 MB (100.00%)

---
+++ solutions/0341-flatten-nested-list-iterator/solution.java
@@ -0,0 +1,58 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+
+import java.util.NoSuchElementException;
+
+public class NestedIterator implements Iterator<Integer> {
+
+    private int index = 0;
+    private List<Integer> integers = new ArrayList<Integer>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        flattenList(nestedList);
+    }
+
+    private void flattenList(List<NestedInteger> nestedList) {
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                integers.add(nestedInteger.getInteger());
+            } else {
+                flattenList(nestedInteger.getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return integers.get(index++);   
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < integers.size();
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
