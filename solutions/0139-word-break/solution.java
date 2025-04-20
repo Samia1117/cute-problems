fba70a59760e08b6d5281055296fa533a11df82c
Samia <48843553+Samia1117@users.noreply.github.com>
2025-04-20 13:08:04 +0000

    Sync LeetCode submission Runtime - 8 ms (44.63%), Memory - 42.7 MB (98.54%)

--- solutions/0139-word-break/solution.java
+++ solutions/0139-word-break/solution.java
@@ -1,32 +1,109 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        
-        LinkedList<Integer> deque = new LinkedList<Integer>();
-        deque.addFirst(0);
+    private List<String> wordDict;
+    private String s;
+    private int[] dp;
 
-        Set<String> words = new HashSet<String>(wordDict);
-        boolean[] seen = new boolean[s.length() + 1];
+    public boolean checkWordBreak(int i) {
+        if (i < 0) {
+            return true;
+        }
 
-        while (!deque.isEmpty()) {
-            int start = deque.pollLast();
-            if (start == s.length()) {
+        if (dp[i] != -1) {
+            // already visited
+            return dp[i] == 1;
+        }
+
+        for (String word: wordDict) {
+            int newWordStart = i - word.length() + 1; 
+            // e.g. i = 10, wordLength = 6, so word.length() is 4. Check that the word:
+            // s[5] + s[6] + s[7] + s[8] + s[9] + s[10] is a word AND that dp[4/word.length()] is true
+            if (newWordStart < 0) {
+                continue;
+            } 
+            if (s.substring(i-word.length() + 1, i+1).equals(word) && checkWordBreak(i-word.length())) {
+                dp[i] = 1;
                 return true;
             }
+        }
 
-            for (int end = start + 1; end <= s.length(); end ++) {
-                if (seen[end]) {
+        dp[i] = 0;
+        return false;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s.length() == 0) {
+            return true;
+        }
+
+        // Let dp[i] = if it is possible to wordBreak ending at s[i]
+        // Return dp[s.length() - 1]
+
+        // Base case: dp[i<0] = true
+        // dp[i] (i > = 0) := if 1) there exists an index j < i, such that s.substring(j, i+1) is a word in the dictionary of words
+        // 2) dp[j-1] is also true (it is possible to wordBreak ending at s[j-1])
+
+        boolean[] dp = new boolean[s.length()];
+        HashSet<String> words = new HashSet<>(wordDict);
+        for (int i=0; i<s.length(); i++) {
+            for (String word: words) {                
+                // System.out.printf("Word = %s\n", word);
+                int endIndex = i - word.length(); // 10 - 6
+                int startIndex = endIndex + 1;
+
+                if (startIndex < 0) {
                     continue;
-                }
-                if (words.contains(s.substring(start, end))) {
-                    deque.addLast(end);
-                    seen[end] = true;
+                } else {
+                    if (startIndex == 0 || dp[endIndex]) { // endIndex must be >= 0 here
+                        // possible to wordBreak with s[:i]
+                        String possibleWord = s.substring(startIndex, i+1);
+                        // System.out.printf("Possible word = %s\n", possibleWord);
+                        if (words.contains(possibleWord)) {
+                            dp[i] = true;
+                            // System.out.printf("DP was filled with a 1 at %d\n", i);
+                            // System.out.println(Arrays.toString(dp));
+                            continue;
+                        }
+                    }
                 }
             }
         }
 
+        return dp[s.length() - 1];
+
+
+        // this.s = s;
+        // this.wordDict = wordDict;
+        // this.dp = new int[s.length()];
+        // Arrays.fill(this.dp, -1);
+        // return checkWordBreak(s.length() - 1);
+
+        /** 
+        * Good approach but fails runtime requirement
+
+        LinkedList<Integer> deque = new LinkedList();
+        deque.addLast(0);
+        HashSet<String> words = new HashSet<>(wordDict);
+        boolean[] seen = new boolean[s.length()+1];
+        seen[0] = true;
+
+        while (!deque.isEmpty()) {
+            int start = deque.pollLast();
+            if (start == s.length()) { 
+                // we have reached a starting point that came right after the close of a word and
+                // this starting point happens to be the end - i.e. we 'word hopped' our way out of 
+                // the initial word 's'
+                return true;
+            }
+            for (int end = start + 1; end <= s.length(); end++) {
+                String possibleWord = s.substring(start, end); 
+                if (words.contains(possibleWord)) {
+                    if (!seen[end]) {
+                        deque.addLast(end); // another possible start for a word
+                    }
+                }
+            }
+        }
         return false;
-
-
-
+        */
     }
 }
