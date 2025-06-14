94f9edcec1bea5b61def06aa345a2da92043d606
Samia <48843553+Samia1117@users.noreply.github.com>
2025-06-14 21:04:55 +0000

    Sync LeetCode submission Runtime - 56 ms (47.32%), Memory - 18.7 MB (69.73%)

--- solutions/1209-design-bounded-blocking-queue/solution.py
+++ solutions/1209-design-bounded-blocking-queue/solution.py
@@ -1,60 +1,31 @@
 from threading import Condition
-from threading import Lock
 
 class BoundedBlockingQueue(object):
 
+    # BoundedBlockingQueue b = BoundedBlockingQueue()
+    # b.enqueue(0)
     def __init__(self, capacity: int):
+        self.capacity = capacity
         self.queue = deque()
-        self.cap = capacity
-
         self.cond = Condition()
-
-        self.enq_lock = Lock()
-        self.deq_lock = Lock()
-
-        self.deq_lock.acquire()
-
-    def enqueue(self, element: int) -> None:
-
-        self.enq_lock.acquire()
-        self.queue.append(element)
+        self.cond = Condition()
         
-        if self.deq_lock.locked():
-            self.deq_lock.release()
-        if len(self.queue) < self.cap:
-            self.enq_lock.release()
-
-        # self.cond.acquire()
-        # while len(self.queue) == self.cap:
-        #     self.cond.wait()
-        
-        # self.queue.append(element)
-        # # print(f'Self.q = {self.queue}')
-        # self.cond.notify_all()
-        # self.cond.release()
+    def enqueue(self, element: int) -> None: 
+        with self.cond: # only enqueue if queue not full
+            # print("acquired cond (enqueue)")
+            while len(self.queue) >= self.capacity:
+                self.cond.wait()
+            self.queue.append(element)
+            self.cond.notifyAll()
 
     def dequeue(self) -> int:
-
-        self.deq_lock.acquire()
-        elt = self.queue.popleft()
-
-        if self.enq_lock.locked():
-            self.enq_lock.release()
-
-        if len(self.queue) > 0:
-            self.deq_lock.release()
-
-        return elt
-
-        # self.cond.acquire()
-        # while len(self.queue) == 0:
-        #     self.cond.wait()
-
-        # elt = self.queue.popleft()  # Note, the elt has already been popped so cannot do anything more about it 
-        # # print(f'Self.q = {self.queue}')
-        # self.cond.notify_all()
-        # self.cond.release()
-        # return elt
+        with self.cond: # only deque if queue empty
+            # print("acquired cond (dequeue)")
+            while len(self.queue) == 0:
+                self.cond.wait()
+            ans = self.queue.popleft()
+            self.cond.notifyAll()
+        return ans
 
     def size(self) -> int:
         return len(self.queue)
