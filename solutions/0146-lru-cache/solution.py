7c9a495997c68b9d27850bcb6b2c75422187bc9b
Samia <48843553+Samia1117@users.noreply.github.com>
2025-06-18 12:10:54 +0000

    Sync LeetCode submission Runtime - 169 ms (17.52%), Memory - 78.8 MB (13.09%)

--- solutions/0146-lru-cache/solution.py
+++ solutions/0146-lru-cache/solution.py
@@ -1,43 +1,73 @@
-import time
+class ListNode:
+    def __init__(self, key, val):
+        self.prev = None
+        self.next = None
+        self.key = key
+        self.val = val
 
 class LRUCache:
+
+    '''
+    Add node to the front (tail) of the linkedlist
+    '''
+    def add(self, node):
+        # pass
+        prev_end = self.tail.prev
+        prev_end.next = node
+        node.prev = prev_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    '''
+    Remove node from ll
+    '''
+    def remove(self, node):
+        prev = node.prev
+        nex = node.next
+
+        if prev != None:
+            prev.next = nex
+        if nex != None: 
+            nex.prev = prev
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        cache = self.cache
-        if key in cache:
-            # update this key's last updated time
-            self.cache[key] = (cache[key][0], time.time())
-            return cache[key][0]
-        return -1
+        if key not in self.cache:
+            return -1
+        
+        node = self.cache[key]
+        # remove it from its prev position in ll
+        self.remove(node)
+        # now make it MRU
+        self.add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        # print("cache: ", self.cache)
-        cache = self.cache
-        if key in cache:
-            self.cache[key] = (value, time.time())
-        else:
-            # new key
-            if len(cache) < self.capacity:
-                self.cache[key] = (value, time.time())
-            else:
-                # print("resizing at cache length, cap = ", [len(cache), self.capacity])
-                # evict the least recently used key
-                oldest_time = sys.maxsize
-                oldest_key = None
-                for k in cache:
-                    if cache[k][1] < oldest_time:
-                        oldest_key = k
-                        oldest_time = cache[k][1]
+        # if key already in cache, then update; else add to tail
+        # if exceed capacity, first remove tail
 
-                self.cache.pop(oldest_key)
-                self.cache[key] = (value, time.time())
-
-
+        if key in self.cache:
+            # update
+            node = self.cache[key]
+            self.remove(node)
         
+        # add this node to cache and to inner linked list
+        node = ListNode(key, value)
+        self.cache[key] = node
+        self.add(node)
+
+        if len(self.cache) > self.capacity:
+            # remove head 
+            lru_node = self.head.next
+            self.remove(lru_node)
+            del self.cache[lru_node.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
