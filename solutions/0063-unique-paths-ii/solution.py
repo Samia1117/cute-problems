c1b2cb3253a371cb2a63210299d53266632afe5d
Samia <48843553+Samia1117@users.noreply.github.com>
2025-06-22 13:05:48 +0000

    Sync LeetCode submission Runtime - 1 ms (34.16%), Memory - 17.8 MB (63.31%)

--- solutions/0063-unique-paths-ii/solution.py
+++ solutions/0063-unique-paths-ii/solution.py
@@ -1,30 +1,40 @@
-class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
-        """
-        :type obstacleGrid: List[List[int]]
-        :rtype: int
-        """
-        numRows = len(obstacleGrid)
-        if numRows == 0:
-            return 0
-        numCols = len(obstacleGrid[0]) # square grid so numCols != 0 
-        if (obstacleGrid[numRows-1][numCols-1] == 1):
-            return 0
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
-        paths = [[ 0 for j in range(numCols)] for i in range(numRows)]
+        # Let # of uniquePaths ending at grid[i][j] be dp[i][j]
+        # we need dp[m-1][n-1]
+
+        # dp[i][j] = dp[i][j-1] # from left + dp[i-1][j] # from top
+        
+        rows = len(obstacleGrid)
+        if rows == 0:
+            return 0
+        cols = len(obstacleGrid[0])
+
+        dp = [[0 for i in range(cols)] for j in range(rows)]
         if obstacleGrid[0][0] == 1:
             return 0
-        else:
-            paths[0][0] = 1
-        for i in range(numRows):
-            for j in range(numCols):
+            
+        dp[0][0] = 1
+
+        for i in range(rows):
+            for j in range(cols):
                 if obstacleGrid[i][j] == 0:
-                    if (i-1) >= 0:
-                        paths[i][j] += paths[i-1][j]
-                    if (j-1 >= 0):
-                        paths[i][j] += paths[i][j-1]
-        return paths[numRows-1][numCols-1]
+                    if i-1 >= 0:
+                        dp[i][j] += dp[i-1][j]
+                    if j-1 >= 0:
+                        dp[i][j] += dp[i][j-1]
+                    
+        return dp[rows-1][cols-1]
 
 
+        # max_sum = rows + cols - 1
+        # for s in reversed(range(max_sum)): # s = sum
+        #     # (2, 2), (1, 2), (2, 1), -> (2, 0), (1, 1), (0, 2)...
+        #     # (3, 3), (3, 2), (2, 3), -> ()
+        #     for j in range(max_sum):
+        #         i = max_sum//2 - j
+        #         j = s - i
+        #         print(f'i, j = {i, j}')
 
-
+        
