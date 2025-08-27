fabbe044d3d03de72cf22e4523919d80e06c610b
Samia <48843553+Samia1117@users.noreply.github.com>
2025-08-27 03:12:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (60.99%)

--- solutions/0198-house-robber/solution.py
+++ solutions/0198-house-robber/solution.py
@@ -1,20 +1,49 @@
+# from functools import cache
+
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        
+    '''
+    Approach 1: Recursion
+    '''
+    def rob_recursive(self, nums):
         n = len(nums)
+        if n == 0:
+            return 0
+        elif n == 1:
+            return nums[0]
+        elif n == 2:
+            return max(nums[0], nums[1])
+        else:
+            a = self.rob_recursive(nums[1:])
+            b = nums[0] + self.rob_recursive(nums[2:])
+            return max(a, b)
+
+    '''
+    Approach 2: Dynamic Programming
+    '''
+    def rob_dp(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
         if n == 1:
             return nums[0]
-
-        dp = [0 for i in range(n)]
-        dp[0] = nums[0]
-
-        max_so_far = dp[0]
-
-        for i in range(1, n):
-            if i > 1:
-                # dp[i] = max(max(dp[:i-1]) + nums[i], max(dp[:i]))
-                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-            else:
-                dp[i] = max(nums[i], dp[0])
+        if n == 2:
+            return max(nums[0], nums[1])
         
-        return max(dp)
+        dp = [0 for i in range(n)]
+        # dp[i] = total reward by robbing houses up to i^th house
+        # Want: dp[n] := total reward by robbing houses up to n^th house
+        # dp[i] = max { 1. dp[i-2] + nums[i] # rob house i; 2. dp[i-1] # don't rob house i}
+
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[n-1]
+
+    def rob(self, nums: List[int]) -> int:
+        # return self.rob_recursive(nums)
+        return self.rob_dp(nums)
+        
