e56d8f4d2c9be66fde3f1262fbd0070c510014f9
Samia <48843553+Samia1117@users.noreply.github.com>
2025-08-29 02:36:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.4 MB (53.29%)

--- solutions/0871-keys-and-rooms/solution.py
+++ solutions/0871-keys-and-rooms/solution.py
@@ -1,43 +1,19 @@
-import numpy as np
-
 class Solution:
-
-    def dfs(self, node, rooms, visited):
-        if visited[node]:
-            return
-        visited[node] = True
-        return
-
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-
-        # create adjacency matrix
-        d = {}
         n = len(rooms)
-        for i in range(n):
-            d[i] = rooms[i]
 
-        print("dict: ", d)
+        rooms_visited = set()
 
-        visited = [False for i in range(n)]
-
-        stack = deque()
-        stack.append(0) # can visit room 0
-        # visited = set([])
-
-        while stack:
-            print("stack", stack)
-            popped = stack.pop()
-            visited[popped] = True
-
-            neighbors = d[popped]
-            for neigh in neighbors:
-                if visited[neigh] == False:
-                    stack.append(neigh)
-        
-        for i in range(n):
-            if not visited[i]:
-                return False
-        return True
-
-
+        remaining = set([0])
+        seen = set([0])
+        i = 0
+        while remaining:
+            next_idx = remaining.pop()
+            for item in rooms[next_idx]:
+                if item not in seen:
+                    remaining.add(item)
+                    seen.add(item)
+            if len(seen) == n:
+                return True
+        return False
         
