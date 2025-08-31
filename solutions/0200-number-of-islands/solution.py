bef4a85b4a876bd00ade705a4c83fc8b878c1b6f
Samia <48843553+Samia1117@users.noreply.github.com>
2025-08-31 15:27:19 +0000

    Sync LeetCode submission Runtime - 239 ms (69.65%), Memory - 20.3 MB (67.42%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -1,64 +1,33 @@
 class Solution:
-    def idx(self, row, col, maxRow, maxCol):
-        return (row * maxCol) + col
+    def isInBounds(self, row, col):
+        return 0 <= row < self.maxRow and 0 <= col < self.maxCol
 
-    def is_in_bounds(self, i, j, maxRow, maxCol):
-        return i >= 0 and j >= 0 and i < maxRow and j < maxCol
+    def dfsAndMark(self, grid, row, col):
+        # dfs
+        grid[row][col] = "X"
+        deltas = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    def dfs_and_mark(self, visited, grid, i, j, maxRow, maxCol):
-        st = deque()
-        st.append([i,j])
-
-        # print("i, j: ", [i,j])
-        while st:
-            popped = st.pop()
-            visited[popped[0]][popped[1]] = True
-            # print("New visited: ", visited)
-            # left
-            left_i, left_j = popped[0], popped[1] - 1
-            # print("left i, j: ", [left_i, left_j])
-            if self.is_in_bounds(left_i, left_j, maxRow, maxCol) and grid[left_i][left_j] == '1' and not visited[left_i][left_j]:
-                st.append([left_i, left_j])
-
-            # right
-            right_i, right_j = popped[0], popped[1] + 1
-            # print("right i, j: ", [right_i, right_j])
-            if self.is_in_bounds(right_i, right_j, maxRow, maxCol) and grid[right_i][right_j] == '1' and not visited[right_i][right_j]:
-                st.append([right_i, right_j])
-
-            # up
-            up_i, up_j = popped[0] - 1, popped[1]
-            # print("up i, j: ", [up_i, up_j])
-            if self.is_in_bounds(up_i, up_j, maxRow, maxCol) and grid[up_i][up_j] == '1' and not visited[up_i][up_j]:
-                st.append([up_i, up_j])
-
-            # down
-            down_i, down_j = popped[0] + 1, popped[1]
-            # print("down i, j: ", [down_i, down_j])
-            if self.is_in_bounds(down_i, down_j, maxRow, maxCol) and grid[down_i][down_j] == '1' and not visited[down_i][down_j]:
-                st.append([down_i, down_j])
-            
-        return
+        for d in deltas:
+            new_row, new_col = row + d[0], col + d[1]
+            if self.isInBounds(new_row, new_col) and grid[new_row][new_col] == "1":
+                self.dfsAndMark(grid, new_row, new_col)
 
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        maxRow = len(grid)
-        if maxRow == 0:
+        m = len(grid) # rows
+        if m == 0:
             return 0
-        maxCol = len(grid[0])
+        n = len(grid[0]) # columns
 
-        visited = [[False for i in range(maxCol)] for j in range(maxRow)]
+        self.maxCol = n
+        self.maxRow = m
 
-        conn_components = 0
-        for i in range(maxRow):
-            for j in range(maxCol):
+        numIslands = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    self.dfsAndMark(grid, i, j)
+                    numIslands += 1
+        
+        return numIslands
 
-                # check connnected components 
-                if grid[i][j] == '1' and not visited[i][j]:
-                    # print("found land at: ", [i, j])
-                    self.dfs_and_mark(visited, grid, i, j, maxRow, maxCol)
-                    conn_components += 1
-                # print("One connected component down. I, j: ", [i,j])
-                        
-        return conn_components
         
