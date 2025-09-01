ef1962ea84fe2014d9128cba582e98ea607d9dce
Samia <48843553+Samia1117@users.noreply.github.com>
2025-09-01 00:24:45 +0000

    Sync LeetCode submission Runtime - 49 ms (87.27%), Memory - 22.1 MB (61.26%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -6,44 +6,23 @@
 #         self.right = None
 
 class Solution:
-        # main idea is: if my right and left both return not none values, then either p or q is on right and the other on left
-        # in that case, i should return the  root
-
-        # otherwise, either left or right is none - so just return the side that's not none
-        
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+
+        # Base case
         if not root:
-            return root
+            return None
+        
         if root.val == p.val or root.val == q.val:
             return root
         
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-        
+
         if left and right:
             return root
+        
         if left:
             return left
+        
         return right
-        
-#         if root in (p, q, None):
-#             return root
-#         r,l = [self.lowestCommonAncestor(child, p, q) for child in (root.left, root.right)]
-        
-#         if r and l:
-#             return root
-#         if not r:
-#             return l
-#         return r
-        
-#         st = deque()
-#         st.append(root)
-#         visited = set()
-        
-#         while st:
-#             p = st.pop()
-            
-            
-        
         
