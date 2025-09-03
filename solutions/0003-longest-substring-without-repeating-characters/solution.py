198d40673164f6850c1ba76e98cd739067628fa6
Samia <48843553+Samia1117@users.noreply.github.com>
2025-09-03 03:57:25 +0000

    Sync LeetCode submission Runtime - 20 ms (29.31%), Memory - 18.1 MB (51.79%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.py
+++ solutions/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,20 +1,45 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
         n = len(s)
-        ans = 0
-        # charToNextIndex stores the index after current character
-        charToNextIndex = {}
+        if n == 0 or n == 1:
+            return n
+        
+        curr_streak = set([])
+        max_seq = 0
+        # left, right = 0, 0
+        left = 0
+# 
 
-        i = 0
-        # try to extend the range [i, j]
-        for j in range(n):
-            print(f'i, j = {i, j}')
-            if s[j] in charToNextIndex:
-                i = max(charToNextIndex[s[j]], i)
-                # i = charToNextIndex[s[j]]
+        for right in range(n):
+            while s[right] in curr_streak:
+                curr_streak.remove(s[left])
+                left += 1
+            curr_streak.add(s[right])
+            max_seq = max(max_seq, (right - left + 1))
+        return max_seq
+        '''
+        while right < n:
+            # max_seq = max(max_seq, len(curr_streak))
+            # print(f'curr_streak = {curr_streak}')
+            # print(f'curr_seq = {s[left:right+1]}')
+            # print(f'max_seq = {max_seq}')
+            max_seq = max((right - left + 1), max_seq)
 
-            ans = max(ans, j - i + 1)
-            charToNextIndex[s[j]] = j + 1
+            if s[right] in curr_streak:
+                while s[left] != s[right]:
+                    curr_streak.remove(s[left])
+                    left += 1
+                # now s[left] == s[right] e.g. a*bca*
+                if left < n:
+                    # remove the duplicate char
+                    curr_streak.remove(s[left])
+                    # shrink the window
+                    left += 1
+            else:
+                # expand the window
+                curr_streak.add(s[right])
+                right += 1
 
-        return ans
+        # max_seq = max(max_seq, len(curr_streak))
+        return max_seq
+        '''
