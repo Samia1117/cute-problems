77ddb7c490707b8956e8bc474417774ea6828a81
Samia <48843553+Samia1117@users.noreply.github.com>
2025-09-18 02:02:33 +0000

    Sync LeetCode submission Runtime - 20 ms (96.79%), Memory - 30.7 MB (88.59%)

--- solutions/0208-implement-trie-prefix-tree/solution.py
+++ solutions/0208-implement-trie-prefix-tree/solution.py
@@ -1,64 +1,33 @@
 class Trie:
 
     def __init__(self):
-        # {'a':{ Trie({b: }), Trie({c:}),... }, 'b':{..}, .. }
         self.trie = {}
-        
 
     def insert(self, word: str) -> None:
- 
-        if len(word) == 0:
-            return
-
-        current_trie = self.trie
-        for ch in word:
-            if ch not in current_trie:
-                current_trie[ch] = {}
-            current_trie = current_trie[ch]
-        
-        # reached end of word. Mark it so: "cat" -> "t":{"#":"#"}
-        current_trie["#"] = "#"
+        trie = self.trie
+        for w in word:
+            if w not in trie:
+                trie[w] = {}
+            trie = trie[w]
+        trie['#'] = {} # mark the end of the word 
 
     def search(self, word: str) -> bool:
-        current_trie = self.trie
-
-        n = len(word)
-        if n == 0:
+        trie = self.trie
+        for w in word:
+            if w not in trie:
+                return False
+            trie = trie[w]
+        if "#" in trie:
             return True
-
-        for i in range(n):
-            ch = word[i]
-            if i == n-1 and ch in current_trie:
-                children = current_trie[ch]
-                if "#" in children:
-                    # This is a word
-                    return True
-                else:
-                    # This is not a word
-                    return False
-            else:
-                if ch in current_trie:
-                    current_trie = current_trie[ch]
-                else:
-                    return False
-        # should never reach here
         return False
-                
 
     def startsWith(self, prefix: str) -> bool:
-
-        current_trie = self.trie
-        if len(prefix) == 0:
-            return True
-
-        for ch in prefix:
-            if ch in current_trie:
-                current_trie = current_trie[ch]
-            else:
+        trie = self.trie
+        for w in prefix:
+            if w not in trie:
                 return False
+            trie = trie[w]
         return True
-
-        
 
 
 # Your Trie object will be instantiated and called as such:
