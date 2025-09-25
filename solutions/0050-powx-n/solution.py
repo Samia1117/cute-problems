0eced5ff8a5c699c81e72c1c225e1c4f841a4c54
Samia <48843553+Samia1117@users.noreply.github.com>
2025-09-25 03:50:37 +0000

    Sync LeetCode submission Runtime - 3 ms (9.70%), Memory - 18.2 MB (49.61%)

--- solutions/0050-powx-n/solution.py
+++ solutions/0050-powx-n/solution.py
@@ -1,22 +1,16 @@
 class Solution:
-
     @cache
     def myPow(self, x: float, n: int) -> float:
-
         if n == 0:
             return 1
-        elif n == 1:
+        if n == 1:
             return x
-        elif n < 0:
+        
+        if n < 0:
             return 1/self.myPow(x, -n)
-            
+        
+        if n % 2 == 0:
+            return self.myPow(x*x, n//2)
         else:
-            if n%2 == 0:
-                return self.myPow(x, n/2) * self.myPow(x, n/2)
-            else:
-                n_half_floor = n//2
-                n_half_ceil = n_half_floor + 1
-
-                return self.myPow(x, n_half_floor) * self.myPow(x, n_half_ceil)
-
+            return x * self.myPow(x*x, (n - 1)//2)
         
