9035a69960b38dd16e6eb34d42f0e490b695aa01
Samia <48843553+Samia1117@users.noreply.github.com>
2025-11-29 00:48:29 +0000

    Sync LeetCode submission Runtime - 369 ms (5.04%), Memory - 23.2 MB (54.15%)

--- solutions/1014-k-closest-points-to-origin/solution.py
+++ solutions/1014-k-closest-points-to-origin/solution.py
@@ -1,18 +1,50 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
-        # distDict = {}
-        distList = []
-        for point in points:
-            distFromOrigin = self.distanceFromOrigin(point)
-            # distDict[(point[0], point[1])] = distFromOrigin
-            distList.append((distFromOrigin, point))
+# from collections import heapq
 
-        # sortedPoints = sorted(distDict.items(), key=lambda x: x[1])
-        sortedPoints = sorted(distList, key=lambda x: x[0])
+class Node:
+    def __init__(self, dist, point):
+        self.dist = dist
+        self.point = point
+
+    def __lt__(self, other):
+        return self.dist > other.dist
+
+    def __repr__(self):
+        return f'{self.dist}, ({self.point})'
+
+class Solution:
+    
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        '''
+        One idea is to create a map of (x,y) from their distance to origin
+        Sort the dictionary by key and return top k values
+
+        Keep a size-k heap, so that runtime will be worst case O(nlogk)
+        Good for a first time attempt, before thinking further
+        '''
         
-        return [tup[1] for tup in sortedPoints[0:k]]
-    
-    
-    def distanceFromOrigin(self, coords):
-        return sqrt((coords[0]*coords[0]) + (coords[1]*coords[1]))
+        heap = []
+        heapq.heapify(heap)
+        heap_size = 0
+
+        for point in points:
+            x, y = point[0], point[1]
+            orig_dist = (x**2 + y**2)**0.5
+            node = Node(orig_dist, (x, y))
+            print(f'Node = {node}')
+
+            heapq.heappush(heap, node)
+            heap_size += 1
+            if heap_size > k:
+                heapq.heappop(heap)
+        
+        # print(f'Heap (size = {len(heap)}) elts in order...')
+        
+        ret = []
+        while heap:
+            elt = heapq.heappop(heap)
+            x, y = elt.point[0], elt.point[1]
+            # print(f'Next elt = ({elt})')
+            ret.append([x, y])
+        
+        return ret[::-1]
+        
