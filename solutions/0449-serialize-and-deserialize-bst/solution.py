1cdd08fa88b13e62d1f6b6d4efc745a6a25c348e
Samia <48843553+Samia1117@users.noreply.github.com>
2026-01-20 01:34:33 +0000

    Sync LeetCode submission Runtime - 71 ms (21.58%), Memory - 23.1 MB (11.88%)

--- solutions/0449-serialize-and-deserialize-bst/solution.py
+++ solutions/0449-serialize-and-deserialize-bst/solution.py
@@ -7,42 +7,51 @@
 
 class Codec:
 
+    ser = ""
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
         """
-        def rserialize(node):
-            if not node:
-                return "#"
-            left = rserialize(node.left)
-            right = rserialize(node.right)
-            # print(f'left, right = {left, right}')
-            return str(node.val) + "," + left + "," + right
+        # traverse the tree 
+        # encode using a pre-order string
 
-        data_str = rserialize(root)
-        print(f'data_str = {data_str}')
-        return data_str
-        
+        res = []
+        def rserialize(root: Optional[TreeNode]):
+
+            if root:
+                res.append(str(root.val))
+                rserialize(root.left)
+                rserialize(root.right)
+            else:
+                res.append("#")
+
+        rserialize(root)
+        return ",".join(res)
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree.
         """
-        def rserialize(data_list, index):
-            if index[0] == len(data_list):
-                print("Problem")
+        index = [0]
+
+        def rdeserialize(data: list[str], index: list[int]) -> Optional[TreeNode]:
+
+            if index[0] >= len(data):
                 return None
-            if data_list[index[0]] == "#":
+
+            if data[index[0]] == "#":
                 index[0] += 1
                 return None
             
-            root_node = TreeNode(int(data_list[index[0]]))
+            root = TreeNode(int(data[index[0]]))
             index[0] += 1
-            root_node.left = rserialize(data_list, index)
-            root_node.right = rserialize(data_list, index)
-            
-            return root_node
+
+            root.left = rdeserialize(data, index)
+            root.right = rdeserialize(data, index)
+
+            return root
         
-        root = rserialize(data.split(","), [0])
-        return root
+        return rdeserialize(data.split(","), index)
+
+
         
 
 # Your Codec object will be instantiated and called as such:
