ba8bcd768c8d7aa4712edb9ea14305fd6c12aba0
Samia <48843553+Samia1117@users.noreply.github.com>
2026-02-01 02:20:22 +0000

    Sync LeetCode submission Runtime - 52 ms (95.32%), Memory - 66.2 MB (98.51%)

--- solutions/1023-time-based-key-value-store/solution.py
+++ solutions/1023-time-based-key-value-store/solution.py
@@ -1,47 +1,32 @@
+from bisect import bisect_right
 class TimeMap:
-
     def __init__(self):
-        self.map = {}
-    
-    def binarySearchLargestTime(self, tups, timestamp):
-        l = 0
-        r = len(tups)-1
-        mid = (l+r)//2
-        maxVal = ""
-        while l!=mid:
-            if tups[mid][0] < timestamp:
-                l = mid
-                maxVal = tups[mid][1]
-            elif tups[mid][0] > timestamp:
-                r = mid
-            else:
-                return timestamp
-            mid = (l+r)//2
-            
-        if tups[r][0]<=timestamp:
-            return tups[r][1]
-        if tups[l][0]<=timestamp:
-            return tups[l][1]
-        return ""
+        self.timemap = {}
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.map:
-            self.map[key] = []
-        self.map[key].append((timestamp, value))
+
+        if key not in self.timemap:
+            self.timemap[key] = [[], []] # parallel lists (timestamps, values)
+
+        self.timemap[key][0].append(timestamp)
+        self.timemap[key][1].append(value)
 
     def get(self, key: str, timestamp: int) -> str:
         
-        if key not in self.map:
+        if key not in self.timemap:
             return ""
-            #raise Exception("key is not in map")
+
+        timestamps = self.timemap[key][0]
+        values = self.timemap[key][1]
+
+        # find the first index where this timestamp could be inserted keeping the timestamp order
+        index = bisect_right(timestamps, timestamp)
+        if index == 0:
+            return ""
         
-        # shrunk = list(filter(lambda x: x[0]<=timestamp, self.map[key]))
-        return self.binarySearchLargestTime(self.map[key], timestamp)
-    
-        if len(shrunk)==0:
-            return ""
-        # sortedtups = sorted(shrunk, key=lambda x: x[0])
-        # return sortedtups[-1][1]
+        return values[index-1]
+
 
 
 # Your TimeMap object will be instantiated and called as such:
