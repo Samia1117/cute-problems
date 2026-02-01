b649038797cdeda71c7a7771af3ff534179c3261
Samia <48843553+Samia1117@users.noreply.github.com>
2026-02-01 02:34:25 +0000

    Sync LeetCode submission Runtime - 7 ms (5.89%), Memory - 22.6 MB (5.34%)

--- solutions/0230-kth-smallest-element-in-a-bst/solution.py
+++ solutions/0230-kth-smallest-element-in-a-bst/solution.py
@@ -1,3 +1,5 @@
+from heapq import heapify, heappush, heappop
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,21 +7,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def node_arr(self, root, arr):
-        if root == None:
-            return arr
-        arr.append(root.val)
-        if root.right != None:
-            right_nodes = self.node_arr(root.right, arr)
-        if root.left != None:
-            left_nodes = self.node_arr(root.left, arr)
-            
-        return arr
-            
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        arr = []
-        all_nodes_arr = self.node_arr(root, arr)
-        sorted_arr = sorted(all_nodes_arr)
-        print(sorted_arr)
+        # traverse the binary tree and gather all the values in an array
+        # or maybe just store a k-heap and pop off values if heap_size > k
+
+        def _dfs(root: Optional[TreeNode], k_heap: list) -> None:
+
+            if not root:
+                return
+            # k_heap.append(root.val)
+            heappush(k_heap, -root.val)
+
+            _dfs(root.left, k_heap)
+            _dfs(root.right, k_heap)
+            # if len(k_heap) > k:
+            #     heappop(k_heap)
         
-        return sorted_arr[k-1]
+        heap = []
+        _dfs(root, heap)
+
+        n = len(heap)
+        for i in range(n-k):
+            heappop(heap)
+        # print(f'heap = {heap}')
+        return heap[0] * -1
+        
