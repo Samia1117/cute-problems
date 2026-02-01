da9df352b85453bb8a11c0e8caa0f3d1a359120a
Samia <48843553+Samia1117@users.noreply.github.com>
2026-02-01 01:46:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20 MB (31.06%)

--- solutions/0102-binary-tree-level-order-traversal/solution.py
+++ solutions/0102-binary-tree-level-order-traversal/solution.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,28 +7,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def populate_dict(self, root, level_dict, level):
-
-        if not root:
-            return
-
-        if level not in level_dict:
-            level_dict[level] = []
-        
-        level_dict[level].append(root.val)
-
-        self.populate_dict(root.left, level_dict, level+1)
-        self.populate_dict(root.right, level_dict, level+1)
-        
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        level_dict = {}
-        self.populate_dict(root, level_dict, 0)
-        sorted_by_level = sorted(level_dict.items(), key=lambda x: x[0])
+        ret = {}
 
-        return [x[1] for x in sorted_by_level]
+        def _dfs(root: Optional[TreeNode], data: dict, level: int) -> None:
+            if not root:
+                return
+            if level not in data:
+                data[level] = []
+            data[level].append(root.val)
+            _dfs(root.left, data, level+1)
+            _dfs(root.right, data, level+1)
 
+        _dfs(root, ret, 0)
         
-
-
-        
+        formatted = ret.values()
+        # print(f'Values = {formatted}')
+        return list(formatted)
