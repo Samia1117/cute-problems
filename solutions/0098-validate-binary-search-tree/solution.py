d631b5f809628730249af99b8b218eb34c6c627b
Samia <48843553+Samia1117@users.noreply.github.com>
2026-02-02 03:37:50 +0000

    Sync LeetCode submission Runtime - 1 ms (45.83%), Memory - 21 MB (23.87%)

--- solutions/0098-validate-binary-search-tree/solution.py
+++ solutions/0098-validate-binary-search-tree/solution.py
@@ -5,21 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def isValidBSTUtil(self, node, minVal, maxVal):
-        
-        if node == None:
-            return True
-
-        if (node.val >= maxVal or node.val <= minVal):
-            return False
-        
-        return self.isValidBSTUtil(node.left, minVal, node.val) and self.isValidBSTUtil(node.right, node.val, maxVal)
-        
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-        if root == None:
-            return True
+
+
+        def isValid(root, low, high):
+            if not root:
+                return True
             
-        return self.isValidBSTUtil(root, -sys.maxsize, sys.maxsize)
+            if root.val <= low or root.val >= high:
+                return False
+
+            return isValid(root.left, low, root.val) and isValid(root.right, root.val, high)
+        
+        return isValid(root, -math.inf, math.inf)
         
